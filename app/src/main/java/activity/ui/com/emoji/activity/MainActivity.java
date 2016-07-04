e64029287e2e29232378a4f80e7ee36978019c7b
gm <452693688@qq.com>
2016-07-04 16:14:21 +0800

    修改代码

--- app/src/main/java/activity/ui/com/emoji/activity/MainActivity.java
+++ app/src/main/java/activity/ui/com/emoji/activity/MainActivity.java
@@ -1,48 +1,33 @@
 package activity.ui.com.emoji.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.SpannableStringBuilder;
 import android.view.View;
-import android.widget.TextView;
 
 import activity.ui.com.emoji.R;
-import activity.ui.com.emoji.utile.EmojiIcon;
-import activity.ui.com.emoji.view.EmojiEditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private EmojiEditText importEt;
-    private TextView importTv;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.main_import_btn).setOnClickListener(this);
-        importEt = (EmojiEditText) findViewById(R.id.main_import_et);
-        importTv = (TextView) findViewById(R.id.main_import_tv);
-
+        findViewById(R.id.main_emoji_btn).setOnClickListener(this);
+        findViewById(R.id.main_editext_btn).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        String importText = importEt.getSendInputMsg();
-        SpannableStringBuilder result = new SpannableStringBuilder();
-        result.append("显示为：");
-        result.append(importEt.getShowInputMsg());
-        result.append("\n发送转换为：" + importText);
-        result.append("\n接收转换为：");
-        result.append(EmojiIcon.convertToEmoji(importText));
-        importTv.setText(result);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        CharSequence msg = importEt.getShowInputMsg();
-        if(msg!=null){
-            importEt.setText(msg);
+        Intent it = new Intent();
+        switch (v.getId()){
+            case R.id.main_emoji_btn:
+                it.setClass(this,EmojiActivity.class);
+                break;
+            case R.id.main_editext_btn:
+                it.setClass(this,EditextActivity.class);
+                break;
         }
+        startActivity(it);
     }
 }
